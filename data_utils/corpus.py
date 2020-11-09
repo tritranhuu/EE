9505886f3457f0904e1cfe7fdc8f90a3d23f884b
tritranhuu <tritranhuu123@gmail.com>
2020-11-09 09:37:12 +0000

    commit_from_colab

--- data_utils/corpus.py
+++ data_utils/corpus.py
@@ -1,75 +1,70 @@
 import gensim
-# from collections import Counter
-# import torch
-# from torchtext.data import Field, BucketIterator, NestedField
-# from torchtext.datasets import SequenceTaggingDataset
-# from torchtext.vocab import Vocab
+from collections import Counter
+import torch
+from torchtext.data import Field, BucketIterator, NestedField
+from torchtext.datasets import SequenceTaggingDataset
+from torchtext.vocab import Vocab
 
-def token_arg(s):
-    return [(t, a) for t, a in st.split(':') for st in s.split()]
+class Corpus(object):
 
-# class Corpus(object):
+  def __init__(self, 
+    # input_folder, min_word_freq, batch_size, wv_file=None, 
+               args
+    ):
+    # list all the fields
+    self.word_field = Field(lower=True)
+    self.tag_field = Field(unk_token=None)
+    self.entity_field = Field(unk_token=None)
+    self.argument_tag_field = Field(unk_token=None)
+    # self.argument_nested_field = 
+    self.char_nesting_field = Field(tokenize=list)
+    self.char_field = NestedField(self.char_nesting_field)
 
-#   def __init__(self, 
-#     # input_folder, min_word_freq, batch_size, wv_file=None, 
-#                args
-#     ):
-#     # list all the fields
-#     self.word_field = Field(lower=True)
-#     self.tag_field = Field(unk_token=None)
-#     self.entity_field = Field(unk_token=None)
-#     self.argument_tag_field = Field(unk_token=None)
-#     # self.argument_nested_field = 
-#     self.char_nesting_field = Field(tokenize=list)
-#     self.char_field = NestedField(self.char_nesting_field)
-
-#     # create dataset using built-in parser from torchtext
-#     self.train_dataset, self.val_dataset, self.test_dataset = SequenceTaggingDataset.splits(
-#         path=args.input_folder,
-#         train="train.csv",
-#         validation="dev.csv",
-#         test="test.csv",
-#         fields=(
-#           (("word", "char"), (self.word_field, self.char_field)), 
-#           ("tag", self.tag_field))
-#     )
-#     # convert fields to vocabulary list
-#     # self.word_field.build_vocab(self.train_dataset.word, min_freq=min_word_freq)
-#     self.tag_field.build_vocab(self.train_dataset.tag)
-#     # create iterator for batch input
+    # create dataset using built-in parser from torchtext
+    self.train_dataset, self.val_dataset, self.test_dataset = SequenceTaggingDataset.splits(
+        path=args.input_folder,
+        train="train.csv",
+        validation="dev.csv",
+        test="test.csv",
+        fields=(
+          (("word", "char"), (self.word_field, self.char_field)), 
+          ("tag", self.tag_field))
+    )
+    # convert fields to vocabulary list
+    # self.word_field.build_vocab(self.train_dataset.word, min_freq=min_word_freq)
+    self.tag_field.build_vocab(self.train_dataset.tag)
+    # create iterator for batch input
     
     
-#     if args.wv_file:
-#         self.wv_model = gensim.models.KeyedVectors.load_word2vec_format(args.wv_file, binary=True)
-#         self.embedding_dim = self.wv_model.vector_size
-#         word_freq = {word: self.wv_model.wv.vocab[word].count for word in self.wv_model.wv.vocab}
-#         word_counter = Counter(word_freq)
-#         self.word_field.vocab = Vocab(word_counter, min_freq=args.min_word_freq)
-#             # mapping each vector/embedding from word2vec model to word_field vocabs
-#         vectors = []
-#         for word, idx in self.word_field.vocab.stoi.items():
-#             if word in self.wv_model.wv.vocab.keys():
-#                 vectors.append(torch.as_tensor(self.wv_model.wv[word].tolist()))
-#             else:
-#                 vectors.append(torch.zeros(self.embedding_dim))
-#         self.word_field.vocab.set_vectors(
-#                 stoi=self.word_field.vocab.stoi,
-#                 # list of vector embedding, orderred according to word_field.vocab
-#                 vectors=vectors,
-#                 dim=self.embedding_dim
-#             )
-#     else:
-#         self.word_field.build_vocab(self.train_dataset.word, min_freq=args.min_word_freq)
-#     self.char_field.build_vocab(self.train_dataset.char)
+    if args.wv_file:
+        self.wv_model = gensim.models.KeyedVectors.load_word2vec_format(args.wv_file, binary=True)
+        self.embedding_dim = self.wv_model.vector_size
+        word_freq = {word: self.wv_model.wv.vocab[word].count for word in self.wv_model.wv.vocab}
+        word_counter = Counter(word_freq)
+        self.word_field.vocab = Vocab(word_counter, min_freq=args.min_word_freq)
+            # mapping each vector/embedding from word2vec model to word_field vocabs
+        vectors = []
+        for word, idx in self.word_field.vocab.stoi.items():
+            if word in self.wv_model.wv.vocab.keys():
+                vectors.append(torch.as_tensor(self.wv_model.wv[word].tolist()))
+            else:
+                vectors.append(torch.zeros(self.embedding_dim))
+        self.word_field.vocab.set_vectors(
+                stoi=self.word_field.vocab.stoi,
+                # list of vector embedding, orderred according to word_field.vocab
+                vectors=vectors,
+                dim=self.embedding_dim
+            )
+    else:
+        self.word_field.build_vocab(self.train_dataset.word, min_freq=args.min_word_freq)
+    self.char_field.build_vocab(self.train_dataset.char)
 
-#     self.train_iter, self.val_iter, self.test_iter = BucketIterator.splits(
-#         datasets=(self.train_dataset, self.val_dataset, self.test_dataset),
-#         batch_size=args.batch_size
-#     ) 
+    self.train_iter, self.val_iter, self.test_iter = BucketIterator.splits(
+        datasets=(self.train_dataset, self.val_dataset, self.test_dataset),
+        batch_size=args.batch_size
+    ) 
     
-#     # prepare padding index to be ignored during model training/evaluation
-#     self.word_pad_idx = self.word_field.vocab.stoi[self.word_field.pad_token]
-#     self.tag_pad_idx = self.tag_field.vocab.stoi[self.tag_field.pad_token]
-#     self.char_pad_idx = self.char_field.vocab.stoi[self.char_field.pad_token]
-
-print(token_arg('Adjudicator-Arg:T2 Defendant-Arg:T3'))
+    # prepare padding index to be ignored during model training/evaluation
+    self.word_pad_idx = self.word_field.vocab.stoi[self.word_field.pad_token]
+    self.tag_pad_idx = self.tag_field.vocab.stoi[self.tag_field.pad_token]
+    self.char_pad_idx = self.char_field.vocab.stoi[self.char_field.pad_token]
